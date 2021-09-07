b42c6d92e90e1cfc698cf81b8dd96f662e991a2e
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-07 11:40:28 +0100

    Brew formula update for goris version v0.3.4

--- goris.rb
+++ goris.rb
@@ -5,29 +5,29 @@
 class Goris < Formula
   desc "A tool to get gopher image!"
   homepage "https://github.com/borischen0203/homebrew-goris"
-  version "0.3.3"
+  version "0.3.4"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/borischen0203/goris/releases/download/v0.3.3/goris_0.3.3_macOS_64bit.tar.gz"
-      sha256 "97de97289ea08c95877837d8b4490f0bc3690627ef1f539eac6edf4777d79c8c"
+      url "https://github.com/borischen0203/goris/releases/download/v0.3.4/goris_0.3.4_macOS_64bit.tar.gz"
+      sha256 "220e26509c2d211aac3b9f6c6dfedb6c5e379449838732d9cc02b324be3c7ca2"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/borischen0203/goris/releases/download/v0.3.3/goris_0.3.3_macOS_ARM64.tar.gz"
-      sha256 "0b8b59f7986be9944b8fc973e525f57caed7daa62a3e9af0848abc576bbe6472"
+      url "https://github.com/borischen0203/goris/releases/download/v0.3.4/goris_0.3.4_macOS_ARM64.tar.gz"
+      sha256 "63dcdb923a8975006d8d10e0ed298b2dde4d5964d9c805a663eb6e37e4cae3ca"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/borischen0203/goris/releases/download/v0.3.3/goris_0.3.3_Linux_64bit.tar.gz"
-      sha256 "3dd19ce99fd69d372e6fa708e9338dba6c3b6f2c3dab1cab092e3c161a52a643"
+      url "https://github.com/borischen0203/goris/releases/download/v0.3.4/goris_0.3.4_Linux_64bit.tar.gz"
+      sha256 "e28452854b0c8af8a738fc8e2900b87dafda81c575f3ec89372b4ce062b43591"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/borischen0203/goris/releases/download/v0.3.3/goris_0.3.3_Linux_ARM64.tar.gz"
-      sha256 "6fb5471bb2a3973b68aedd12af02fc452ef4bace030a77f23f1da0d7448fd51e"
+      url "https://github.com/borischen0203/goris/releases/download/v0.3.4/goris_0.3.4_Linux_ARM64.tar.gz"
+      sha256 "88543f203699591f2b5ebc2b37d9346590532f9251b421c91eb04152e2c38f0e"
     end
   end
 
